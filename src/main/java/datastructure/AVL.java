3fc591b1158b47eae168436e0e614072020a9494
Abhishek Sengupta <abhishek.sengupta047@gmail.com>
2024-01-07 00:01:08 +0530

    deletion froM AVL

--- src/main/java/datastructure/AVL.java
+++ src/main/java/datastructure/AVL.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import static java.lang.Math.max;
+
 public class AVL {
 
     Node root;
@@ -71,7 +73,7 @@ public class AVL {
 
         // After successful node assignment , increase the height of the current node
         // height of current node = max height of any child + 1 (counting himself)
-        node.nodeHeight = 1 + Math.max(height(node.left),height(node.right));
+        node.nodeHeight = 1 + max(height(node.left),height(node.right));
 
         int nodeBalance = getBalance(node);
 
@@ -124,8 +126,8 @@ public class AVL {
         Y.left = T2;
 
         // Update the heights after the rearrangement
-        Y.nodeHeight = 1 + Math.max(height(Y.left),height(Y.right)); // Imp to update Y first
-        X.nodeHeight = 1 + Math.max(height(X.left),height(Y.right));
+        Y.nodeHeight = 1 + max(height(Y.left),height(Y.right)); // Imp to update Y first
+        X.nodeHeight = 1 + max(height(X.left),height(Y.right));
 
         return X; //Important to send X
     }
@@ -153,8 +155,8 @@ public class AVL {
         X.right = T2;
 
         // Update the heights after the rearrangement
-        X.nodeHeight = 1 + Math.max(height(X.left),height(X.right)); // Imp to update X first
-        Y.nodeHeight = 1 + Math.max(height(Y.left),height(Y.right));
+        X.nodeHeight = 1 + max(height(X.left),height(X.right)); // Imp to update X first
+        Y.nodeHeight = 1 + max(height(Y.left),height(Y.right));
 
         return Y; //Important to send Y
     }
@@ -179,6 +181,113 @@ public class AVL {
         printInOrderTraversal(node.left);
         System.out.println(node.key);
         printInOrderTraversal(node.right);
+    }
+
+    /* Given a non-empty binary search tree, return the
+   node with minimum key value found in that tree.
+   Note that the entire tree does not need to be
+   searched. */
+    Node minValueNode(Node node)
+    {
+        Node current = node;
+
+        /* loop down to find the leftmost leaf */
+        while (current.left != null)
+            current = current.left;
+
+        return current;
+    }
+
+    Node deleteNode(Node root, int key)
+    {
+        // STEP 1: PERFORM STANDARD BST DELETE
+        if (root == null)
+            return root;
+
+        // If the key to be deleted is smaller than
+        // the root's key, then it lies in left subtree
+        if (key < root.key)
+            root.left = deleteNode(root.left, key);
+
+            // If the key to be deleted is greater than the
+            // root's key, then it lies in right subtree
+        else if (key > root.key)
+            root.right = deleteNode(root.right, key);
+
+            // if key is same as root's key, then this is the node
+            // to be deleted
+        else
+        {
+
+            // node with only one child or no child
+            if ((root.left == null) || (root.right == null))
+            {
+                Node temp = null;
+                if (temp == root.left)
+                    temp = root.right;
+                else
+                    temp = root.left;
+
+                // No child case
+                if (temp == null)
+                {
+                    temp = root;
+                    root = null;
+                }
+                else // One child case
+                    root = temp; // Copy the contents of
+                // the non-empty child
+            }
+            else
+            {
+
+                // node with two children: Get the inorder
+                // successor (smallest in the right subtree)
+                Node temp = minValueNode(root.right);
+
+                // Copy the inorder successor's data to this node
+                root.key = temp.key;
+
+                // Delete the inorder successor
+                root.right = deleteNode(root.right, temp.key);
+            }
+        }
+
+        // If the tree had only one node then return
+        if (root == null)
+            return root;
+
+        // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+        root.nodeHeight = max(height(root.left), height(root.right)) + 1;
+
+        // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to check whether
+        // this node became unbalanced)
+        int balance = getBalance(root);
+
+        // If this node becomes unbalanced, then there are 4 cases
+        // Left Left Case
+        if (balance > 1 && getBalance(root.left) >= 0)
+            return rotateRight(root);
+
+        // Left Right Case
+        if (balance > 1 && getBalance(root.left) < 0)
+        {
+            root.left = rotateLeft(root.left);
+            return rotateRight(root);
+        }
+
+        // Right Right Case
+        if (balance < -1 && getBalance(root.right) <= 0)
+            return rotateLeft(root);
+
+        // Right Left Case
+        if (balance < -1 && getBalance(root.right) > 0)
+        {
+            root.right = rotateRight(root.right);
+            return rotateLeft(root);
+        }
+
+        return root;
     }
 
 
